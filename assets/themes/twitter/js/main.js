686889e87f97f6192dfc83f7c9cd28f1fa8c9a96
Jurby <urbanek.jaroslav@gmail.com>
2014-02-19 22:52:14 +0100

    Update main.js

--- assets/themes/twitter/js/main.js
+++ assets/themes/twitter/js/main.js
@@ -4,5 +4,9 @@
 		$(document.links).filter(function() {
 		    return this.hostname != window.location.hostname;
 		}).attr('target', '_blank');
+		
+		$('.x-frame').each(function() {
+		  $(this).after('<iframe width="870" height="500" src="' + ($(this).attr('data-src')) + '" frameborder="0"></iframe>"');
+		});
 	});
 })(window, document, jQuery)
